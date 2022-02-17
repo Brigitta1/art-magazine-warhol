41f62bbedd0e8c6764278aa97da9bb8dfaa4e1e2
peti1000 <petije@gmail.com>
2022-02-17 20:25:03 +0100

    implement readImage

--- src/main/java/com/codecool/fileshare/repository/ImageJdbcRepository.java
+++ src/main/java/com/codecool/fileshare/repository/ImageJdbcRepository.java
@@ -11,7 +11,9 @@ import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.UUID;
 
 
@@ -30,11 +32,12 @@ public class ImageJdbcRepository implements ImageRepository{
         String[] parts = content.split(delims);
         String imageString = parts[1];
         byte[] imageByteArray = Base64.decode(imageString);
+
         InputStream is = new ByteArrayInputStream(imageByteArray);
         String mimeType = null;
         String fileExtension = null;
         try {
-            mimeType = URLConnection.guessContentTypeFromStream(is); //mimeType is something like "image/jpeg"
+            mimeType = URLConnection.guessContentTypeFromStream(is);
             String delimiter="[/]";
             String[] tokens = mimeType.split(delimiter);
             fileExtension = tokens[1];
@@ -52,18 +55,34 @@ public class ImageJdbcRepository implements ImageRepository{
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return id;
         // implement store image in database here
         // content is base64 coded image file
         // generate and return uuid of stored image
         // https://www.base64-image.de/
         // https://codebeautify.org/base64-to-image-converter
-        return id;
     }
 
     @Override
     public String readImage(String uuid) {
+        UUID uuid1 = UUID.fromString(uuid);
+        String query = "SELECT content FROM image WHERE id = ?";
+        byte[] arr = new byte[1];
+        arr = Base64.encode(arr);
+        String result = null;
+        try (Connection connection = db.getDataSource().getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setObject(1, uuid1);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                result = new String(Base64.encode(rs.getBytes("content")));
+            }
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
         // implement readImage from database here
         // return base64 encoded image
-        return null;
     }
 }
