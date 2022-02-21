104dea35c929827681dd8801a000323437d62970
peti1000 <petije@gmail.com>
2022-02-21 14:05:21 +0100

    update version to BETA mod database connection credentials

--- src/main/java/com/codecool/fileshare/FileshareApplication.java
+++ src/main/java/com/codecool/fileshare/FileshareApplication.java
@@ -1,20 +1,25 @@
 package com.codecool.fileshare;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class FileshareApplication extends SpringBootServletInitializer {
+public class FileshareApplication /*extends SpringBootServletInitializer*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(FileshareApplication.class, args);
+        
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(FileshareApplication.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(FileshareApplication.class);
+//    }
 
 }
--- src/main/java/com/codecool/fileshare/repository/DB.java
+++ src/main/java/com/codecool/fileshare/repository/DB.java
@@ -1,25 +1,25 @@
 package com.codecool.fileshare.repository;
 
 import org.postgresql.ds.PGSimpleDataSource;
-
 import java.sql.SQLException;
 
+
 public class DB {
+
     public PGSimpleDataSource getDataSource() throws SQLException {
         PGSimpleDataSource ds = new PGSimpleDataSource();
 
-//        ds.setServerNames(new String[]{"3.125.49.36"});
-//        ds.setDatabaseName("postgres");
-//        ds.setUser("user");
-//        ds.setPassword("USER123");
-
-        ds.setDatabaseName("warhol");
-        ds.setUser("postgres");
-        ds.setPassword("admin");
+        String name = "postgres";
+        ds.setDatabaseName(name);
+        String userName = "user";
+        ds.setUser(userName);
+        String password = "USER123";
+        ds.setPassword(password);
 
         ds.getConnection().close();
         return ds;
     }
+
 }
 
 
